a65883be97893c139c431525942c890920b32c4d
Antra Tripathi <antra_tripathi@yahoo.com>
2022-04-05 18:46:13 +0100

    summary features

--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from app import app
 from user.models import User
 from mlmodels.model_api import MlModels
+from user.summary import BERTSummarizer
 
 
 @app.route('/user/signup', methods=['POST'])
@@ -22,3 +23,17 @@ def login():
 @app.route('/mlmodel/getmodel', methods=['GET'])
 def get_model():
     return MlModels().get_model()
+
+@app.route('/dashboard/summarizer/', methods=['POST'])
+def get_summary():
+     if request.method == 'POST':
+        file = request.files['file']
+        text= str(file.read())
+        print(text)
+        res = BERTSummarizer(text)
+        print(res)
+        return('Task Done')
+
+
+
+
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+##importing the app from app.py file
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
